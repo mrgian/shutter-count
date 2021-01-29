3792cbd8fb400c33b879be5e230a98b0b572fbe8
mrgian <gianmatteo.palmieri@gmail.com>
2021-01-29 20:47:38 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shutter_count/screens/main/main_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shutter Count',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        //visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
---
+++ lib/screens/main/main_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff1f1f1f),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [Title(), Expanded(child: Container()), BottomButton()],
+        ),
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: 30),
+          child: Center(
+            child: Text(
+              'Shutter Count',
+              style: TextStyle(
+                  fontFamily: 'Inter',
+                  color: Color(0xfff4f4f4),
+                  fontSize: 40,
+                  fontWeight: FontWeight.w800),
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(top: 10, left: 70, right: 70),
+          child: Center(
+            child: Text(
+              'Find out how many pictures your camera has taken',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontFamily: 'Inter',
+                  color: Color(0xffb7b7b7),
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class BottomButton extends StatelessWidget {
+  const BottomButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 10, left: 40, right: 40),
+              child: Center(
+                child: Text(
+                  'Take a picture with your camera and open the RAW file with this button',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: 'Inter',
+                      color: Color(0xfff4f4f4),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Color(0xfff4f4f4),
+                  borderRadius: BorderRadius.circular(20)),
+              margin: EdgeInsets.only(top: 25, bottom: 25),
+              width: 200,
+              height: 70,
+              child: Center(
+                child: Text(
+                  'Select image',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: 'Inter',
+                      color: Color(0xff1f1f1f),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
